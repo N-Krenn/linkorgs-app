9a48ebe2b074c81df0cb78073011bacb9276535c
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2020-09-10 13:49:34 +0200

    Updated to PDO

--- app/src/addlink.php
+++ app/src/addlink.php
@@ -1,22 +1,15 @@
 <?php
+	include 'database.php';
 	$ckan_api = $_POST['ckan_api'];
 	$wikilink = $_POST['wikilink'];
 	$ip = $_SERVER['REMOTE_ADDR'];
 	
 	//need PDO Sec
-	
-	$dbconn = pg_connect("host=localhost dbname=linkorgs user=linkorgs password=test123123")
-			or die('Failure: ' . pg_last_error());
-
-		// Query
-		$query = "INSERT INTO links (fk_ckan_api, wiki_url, ip) VALUES ('$ckan_api', '$wikilink', '$ip')";    ## Make SQL Injection Proof / use PDOs
-		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-		
-	    //Free Space
-		pg_free_result($result);
-
-		// Close Connection
-		pg_close($dbconn);
-		
+	$statement = $conn->prepare("INSERT INTO links (fk_ckan_api, wiki_url, ip) VALUES (:ckan_api, :wikilink, :ip)");
+				$statement->execute(array(
+				    "ckan_api" => $ckan_api,
+				    "wikilink" => $wikilink,
+				    "ip" => $ip
+				));
 		echo "success";
 ?>
