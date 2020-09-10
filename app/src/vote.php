cb3c8ed473f5f259b84cd301c5cf3da1b9bf07ca
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2020-09-10 13:51:16 +0200

    Updated to PDO

--- app/src/vote.php
+++ app/src/vote.php
@@ -3,18 +3,12 @@
 	$direction = $_POST['direction'];
 	$ip = $_SERVER['REMOTE_ADDR'];
 	
-	$dbconn = pg_connect("host=localhost dbname=linkorgs user=linkorgs password=test123123")
-			or die('Failure: ' . pg_last_error());
-
-		// Query
-		$query = "INSERT INTO votes (fk_link_id, direction, ip) VALUES ('$linkid', '$direction', '$ip')";    ## Make SQL Injection Proof / use PDOs
-		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-		
-	    //Free Space
-		pg_free_result($result);
-
-		// Close Connection
-		pg_close($dbconn);
+	$statement = $conn->prepare("INSERT INTO votes (fk_link_id, direction, ip) VALUES (:linkid, :direction, :ip)");
+		$statement->execute(array(
+			"linkid" => $linkid,
+			"direction" => $direction,
+			"ip" => $ip
+		));
 		
 		echo "success";
 ?>
