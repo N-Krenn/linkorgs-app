f36ded3e7495e561e9e9b7adb2c86adcc38eb941
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2020-12-09 20:46:44 +0100

    useragent added and SQL Check for votes

--- app/src/vote.php
+++ app/src/vote.php
@@ -3,12 +3,15 @@
 	$linkid = $_POST['linkid'];
 	$direction = $_POST['direction'];
 	$ip = $_SERVER['REMOTE_ADDR'];
+	$useragent = $_SERVER['HTTP_USER_AGENT']
 	
-	$statement = $conn->prepare("INSERT INTO linkorgs.votes (fk_link_id, direction, ip) VALUES (:linkid, :direction, :ip)");
+	#statement enforces check of already preexisting votes here. 
+	$statement = $conn->prepare("INSERT INTO linkorgs.votes (fk_link_id, direction, ip, useragent) SELECT :linkid, :direction, :ip, :useragent WHERE NOT EXISTS(SELECT 1 FROM linkorgs.votes WHERE ip = :ip AND useragent = :useragent AND fk_link_id = :linkid AND created_at >= NOW() - '24 hours'::INTERVAL);");
 		$statement->execute(array(
 			"linkid" => $linkid,
 			"direction" => $direction,
-			"ip" => $ip
+			"ip" => $ip,
+			"useragent" => $useragent
 		));
 		
 		echo "success";
