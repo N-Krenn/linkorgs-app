9109f3afdd9beae16b8ad3626ffaa8b72134f1d5
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2020-12-09 22:10:51 +0100

    Bugfix

--- app/src/addlink.php
+++ app/src/addlink.php
@@ -5,7 +5,7 @@
 	$ip = $_SERVER['REMOTE_ADDR'];
 	$useragent = $_SERVER['HTTP_USER_AGENT'];
 	
-	$statement = $conn->prepare("INSERT INTO linkorgs.links (fk_ckan_api, wiki_url, ip, useragent) SELECT :ckan_api, :wikilink, :ip, :useragent WHERE NOT EXISTS(SELECT 1 FROM linkorgs-links WHERE ip = :ip AND useragent = :useragent AND fk_ckan_api = :ckan_api AND created_at >= NOW()-'24 hours'::INTERVAL);");
+	$statement = $conn->prepare("INSERT INTO linkorgs.links (fk_ckan_api, wiki_url, ip, useragent) SELECT :ckan_api, :wikilink, :ip, :useragent WHERE NOT EXISTS(SELECT 1 FROM linkorgs.links WHERE ip = :ip AND useragent = :useragent AND fk_ckan_api = :ckan_api AND created_at >= NOW() - '24 hours'::INTERVAL);");
 				$statement->execute(array(
 				    "ckan_api" => $ckan_api,
 				    "wikilink" => $wikilink,
