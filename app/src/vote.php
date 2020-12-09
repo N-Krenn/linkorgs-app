75b459c0e252ab7bf8819edf74764275501e025c
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2020-12-09 21:14:56 +0100

    Bugfix

--- app/src/vote.php
+++ app/src/vote.php
@@ -3,7 +3,7 @@
 	$linkid = $_POST['linkid'];
 	$direction = $_POST['direction'];
 	$ip = $_SERVER['REMOTE_ADDR'];
-	$useragent = $_SERVER['HTTP_USER_AGENT']
+	$useragent = $_SERVER['HTTP_USER_AGENT'];
 	
 	#statement enforces check of already preexisting votes here. 
 	$statement = $conn->prepare("INSERT INTO linkorgs.votes (fk_link_id, direction, ip, useragent) SELECT :linkid, :direction, :ip, :useragent WHERE NOT EXISTS(SELECT 1 FROM linkorgs.votes WHERE ip = :ip AND useragent = :useragent AND fk_link_id = :linkid AND created_at >= NOW() - '24 hours'::INTERVAL);");
