4c4055052290b65e959d973c3a0f98e5f39146c3
N-Krenn <62018182+N-Krenn@users.noreply.github.com>
2021-03-01 21:29:00 +0100

    Syntax + Permission Update for psql deployment

--- script/CreateTable.sql
+++ script/CreateTable.sql
@@ -19,7 +19,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE linkorgs.datasets (
     ckan_api text NOT NULL,
-    portallink text DEFAULT 'err0r'::text
+    portallink text DEFAULT 'err0r'::text,
     metadata jsonb NOT NULL,
     name text DEFAULT 'err0r'::text
 );
@@ -255,3 +255,4 @@ CREATE USER linkorgs WITH PASSWORD 'test123123';
 GRANT CONNECT ON DATABASE linkorgs TO linkorgs;
 GRANT USAGE ON SCHEMA linkorgs TO linkorgs;
 GRANT SELECT,UPDATE,INSERT ON ALL TABLES IN SCHEMA linkorgs TO linkorgs;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA linkorgs TO linkorgs;
